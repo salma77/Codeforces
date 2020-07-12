3fb6edbc94f8cafa8bc16264ff41e6bc978509f0
salma77 <47184333+salma77@users.noreply.github.com>
2020-07-12 02:54:59 +0200

    Update 405A.cpp

--- 405A.cpp
+++ 405A.cpp
@@ -7,13 +7,10 @@ int main()
 	int n;
 	cin >> n;
 	vector< int > a(n);
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		cin >> a[i];
-	}
 	sort(a.begin(), a.end());
-	for (int i = 0; i < n; i++) {
-		cout << a[i]<<" ";
-	}
-	
-	system("pause");
+	for (int i = 0; i < n; i++) 
+		cout << a[i] << " ";
+	return 0;
 }
