a84b3953a35ddd367461d1a9086ca93467802907
salma77 <47184333+salma77@users.noreply.github.com>
2020-07-12 19:12:36 +0200

    Update 141A.cpp

--- 141A.cpp
+++ 141A.cpp
@@ -5,18 +5,13 @@ using namespace std;
 int main()
 {
 	string host, gifted, letter, mash;
-	getline(cin, host);
-	getline(cin, gifted);
-	getline(cin, letter);
+	cin >> host >> gifted >> letter;
 	mash = host + gifted;
 	sort(mash.begin(), mash.end());
 	sort(letter.begin(), letter.end());
-
-
 	if (mash==letter)
 		cout << "YES";
 	else
 		cout << "NO";
-
-	system("pause");
+	return 0;
 }
