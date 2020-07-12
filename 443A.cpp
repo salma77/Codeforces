9800690fc5422d6e6af83350c22c5904ab650f45
salma77 <47184333+salma77@users.noreply.github.com>
2020-07-12 02:57:57 +0200

    Update 443A.cpp

--- 443A.cpp
+++ 443A.cpp
@@ -1,14 +1,13 @@
-#include "pch.h"
 #include <iostream>
 #include<string>
 using namespace std;
 int main()
 {
 	string s;
-	getline(cin, s);
+	cin >> s;
 	int n = 0;
 	for (int i = 0; i < s.length(); i++) {
-		if(s[i]!=',' && s[i]!=' ')
+		if(s[i] != ',' && s[i] != ' ')
 			for (int j = 0; j < s.length(); j++) {
 				if (s[i] == s[j])
 					continue;
@@ -17,5 +16,5 @@ int main()
 			}
 	}
 	cout << n;
-	system("pause");
+	return 0;
 }
