bfd7dcb25e21edd0a17640594a393b36d646e9cf
salma77 <47184333+salma77@users.noreply.github.com>
2020-07-12 19:14:29 +0200

    Update 34A.cpp

--- 34A.cpp
+++ 34A.cpp
@@ -1,13 +1,11 @@
-#include "pch.h"
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 int main()
 {
-	int n,min=10000000,j,k;
+	int n, min=10000000, j, k;
 	int m[100];
-	cin >> n;
-	cin >> m[0];
+	cin >> n >> m[0];
 	for (int i = 1; i < n; i++) {
 		cin >> m[i];
 		if (abs(m[i] - m[i - 1]) < min) {
@@ -20,5 +18,5 @@ int main()
 		cout << "1 " << n << "\n";
 	else
 		cout << j << " " << k << "\n";
-	system("pause");
+	return 0;
 }
