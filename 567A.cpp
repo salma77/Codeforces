65b13c9d5ddc608e1a346f529248b8d2ccd43c4d
salma77 <salmaabdelfatah77@gmail.com>
2021-04-17 19:37:08 +0200

    resolved 567A

--- 567A.cpp
+++ 567A.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-int main()
-{
-	int n, x[100000], max, min;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> x[i];
-	min = x[0];
-	max = x[0];
-	for (int i = 0; i < n; i++) {
-		for (int j= 0; j < n-1 ; j++) {
-			if (i != j) {
-				if (abs(x[i] - x[j]) > max)
-					max = abs(x[i] - x[j]);
-				if (abs(x[i] - x[j]) < min)
-					min = abs(x[i] - x[j]);
-			}
-		}
-		cout << min << " " << max << "\n";
-	}
-	return 0;
+int main(){
+    int n, arr[100000], min, max;
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    for(int i=0;i<n;i++){
+        max = ((arr[i] - arr[0])>(arr[n-1] - arr[i]))? (arr[i] - arr[0]):(arr[n-1] - arr[i]);
+        if(i == n-1)
+            min = abs(arr[i]-arr[i-1]);
+        else if(i == 0)
+            min = abs(arr[i+1]-arr[i]);
+        else
+            min = abs(arr[i+1]-arr[i]) < abs(arr[i]-arr[i-1])? abs(arr[i]-arr[i+1]):abs(arr[i]-arr[i-1]);
+        cout<< min <<" " << max << "\n";
+    }
+    return 0;
 }
